c34e1efbb7c7a16e2a75ea96a7961b710f0c13f7
Pascal Scheidegger <paschcua@hispeed.ch>
2017-09-11 20:19:29 +0200

    added await async firebase methods

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Alert, AppRegistry, StatusBar } from 'react-native'
+import { Alert, AppRegistry, AsyncStorage, StatusBar } from 'react-native'
 import { Container, Content, Button, Text } from 'native-base'
 import { StackNavigator } from 'react-navigation'
 import { Col, Row, Grid } from 'react-native-easy-grid'
@@ -29,17 +29,61 @@ class HomeScreen extends React.Component {
     loadFirebaseOnce()
   }
 
-  setFirebase(){
-    const timestamp = new Date().getTime()
-    firebase.database().ref('users/'+timestamp).set({
-      username: 'Pascal',
-      timestamp: timestamp,
-      email: 'aaa@bbb.ch'
+  async setFirebase(){
+    // Set user profile data
+    const timestamp = new Date().getTime().toString()
+    const randomNumber = (Math.round(Math.random() * (100000 - 1)) + 1).toString()
+
+    const userid = timestamp+randomNumber
+    const username = 'pascy'
+    const email = 'paschcua@hhhhh.com'
+
+    // Store Firebase data
+    firebase.database().ref('users/'+userid).set({
+      userid: userid,
+      username: username,
+      email: email
     })
+    // Store Localstorage persistent data
+    try {
+      await AsyncStorage.setItem('ls_userid', userid)
+      await AsyncStorage.setItem('ls_username', username)
+      await AsyncStorage.setItem('ls_email', email)
+    } catch (error) {
+      Alert.alert(error)
+    }
   }
 
-  getData(cb) {
-    firebase.database().ref('users/').on('value', snap => cb(snap.val()))
+  async getData(cb) {
+    try {
+      const ls_userid = await AsyncStorage.getItem('ls_userid')
+      if (ls_userid !== null){
+        firebase.database().ref('users/'+ls_userid).on('value', snap => cb(snap.val().email))
+      }
+    } catch (error) {
+      Alert.alert(JSON.stringify(error))
+    }
+  }
+
+  async updateData() {
+    const ls_userid = await AsyncStorage.getItem('ls_userid')
+    const ls_username = 'new pascy!'
+    const ls_email = 'paschcua@hhhhh.ababababa'
+
+    // Update Firebase Data
+    try {
+      let getUserdata = firebase.database().ref('users/'+ls_userid);
+      getUserdata.update({ username: 'Ada', email: 'ppp@ppp.ch' });
+    } catch (error) {
+      Alert.alert(error)
+    }
+    // Update Localstorage Data
+    try {
+      await AsyncStorage.setItem('ls_username', ls_username)
+      await AsyncStorage.setItem('ls_email', ls_email)
+    } catch (error) {
+      Alert.alert(error)
+    }
   }
 
   render() {
@@ -60,8 +104,15 @@ class HomeScreen extends React.Component {
                   </Button>
                 </Col>
                 <Col>
-                  <Button block onPress={() => this.getData((val) => Alert.alert(JSON.stringify(val)))} title="getFirebase">
+                  <Button block onPress={() => this.getData((val) => Alert.alert(JSON.stringify(val)) )} title="getFirebase">
                     <Text>getFirebase</Text>
+                  </Button>
+                </Col>
+              </Grid>
+              <Grid>
+                <Col>
+                  <Button block onPress={() => this.updateData()} title="updateFirebase">
+                    <Text>updateFirebase</Text>
                   </Button>
                 </Col>
               </Grid>
